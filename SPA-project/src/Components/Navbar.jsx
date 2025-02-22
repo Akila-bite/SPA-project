04e2cd26d541585332cbe59c243fb8ce8648b0e2
Nompumelelo <akilaprofvictoria@gmail.com>
2025-02-22 13:34:27 +0200

    added Navbar code to see if there were any issues with linking between files

---
+++ SPA-project/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Foodify</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/favorites">Favorites</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+
